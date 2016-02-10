692fc645f0bdde4f23034880d11a2cc89102d1c5
chintankhatri <chintankhatri7@hotmail.com>
2016-02-10 11:26:51 -0500

    Added function to disconnect database and documentation for other class and functions

--- class.php
+++ class.php
@@ -12,10 +12,24 @@ class database {
     private $host = 'localhost';
     private $dbname = 'pha';
     
+    /**
+     * function connects mysql database
+     * @access public
+     * @author chintan khatri <chintan.khatri@hotmail.com>
+     * @return \PDO
+     */
     public function connect() {
-        $db = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname.'', $this->username, $this->password);
+        $db = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname . '', $this->username, $this->password);
         return $db;
     }
     
+    /**
+     * function close the database connection with mysql
+     */
+    public function disconnect() {
+
+        mysql_close();
+    }
+
 }
 ?>
